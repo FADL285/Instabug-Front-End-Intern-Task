aad2f56c29007a72af3c26aee3e930c592ba7530
Mohamed Fadl <mohamed.fadl2852@gmail.com>
2022-05-31 06:05:15 +0200

    Create 404 & welcome view 🚀👋

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,5 +1,29 @@
 <template>
-  <div>404</div>
+  <div id="p404" class="flex-center align-center flex-col">
+    <img src="../assets/images/404-illustration.png" alt="404 Illustration" />
+
+    <div class="text-center">
+      <h1>404 - Page Not Found</h1>
+      <p>Sorry, that page doesn't exists. What would like to do?</p>
+
+      <router-link :to="{ name: 'welcome' }" class="btn-primary"
+        >Go Back to Homepage</router-link
+      >
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+#p404 {
+  min-height: 100vh;
+  gap: 5rem;
+
+  h1 {
+    color: var(--primary-color-dark);
+  }
+
+  p {
+    margin-block: var(--font-small);
+  }
+}
+</style>
--- src/views/WelcomeView.vue
+++ src/views/WelcomeView.vue
@@ -1,7 +1,25 @@
 <template>
-  <div class="home">
+  <div id="welcome" class="flex-center align-center flex-col">
     <img alt="Vue logo" src="../assets/logo.svg" />
+    <div class="text-center">
+      <h2>Hey Fadl, Welcome to BugSquad!</h2>
+      <button class="btn-primary">Log out</button>
+    </div>
   </div>
 </template>
 
-<script setup></script>
+<style lang="scss">
+#welcome {
+  min-height: 100vh;
+  gap: 1rem;
+
+  img {
+    width: 250px;
+  }
+
+  h2 {
+    margin-block: 1.75rem;
+    font-size: calc(var(--font-large) * 1.5);
+  }
+}
+</style>
